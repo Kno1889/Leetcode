4a2d535e6d841881c3027fe53ad80527cc1b5680
Kno1889 <khaled.hossam1889@gmail.com>
2023-02-05 02:17:21 -0500

    Fixed time limit exception in problem 438

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -1,9 +1,14 @@
-class Solution:
+def getCharacterCount(s: str) -> Dict[str, int]:
+        return {character: s.count(character) for character in set(s)}
+
+class Solution:    
     def findAnagrams(self, s: str, p: str) -> List[int]:
         indices_list = []
 
+        p_chars = getCharacterCount(p)
+
         for i in range(len(s)):
-            if sorted(s[i: i + len(p)]) == sorted(p):
+            if getCharacterCount(s[i: i + len(p)]) == p_chars:
                 indices_list.append(i)
         
         return indices_list
