91edb481c515f3db91cebe90bbeb1f0264725742
Kno1889 <khaled.hossam1889@gmail.com>
2023-02-12 23:05:22 -0500

    More memory efficient soln - but slower

--- 1523. Count Odd Numbers in an Interval Range.py
+++ 1523. Count Odd Numbers in an Interval Range.py
@@ -1,8 +1,6 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        num_odd = (high - low) // 2
-
         if (high % 2 == 1) or (low % 2 == 1):
-            num_odd += 1
+            return 1 + (high - low) // 2
 
-        return num_odd
+        return (high - low) // 2
