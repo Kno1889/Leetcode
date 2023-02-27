23e4d5ca14af5c761495245e9447b2b2c80d6ca1
Kno1889 <khaled.hossam1889@gmail.com>
2023-02-27 15:30:43 -0500

    Problem 832

---
+++ 832. Flipping an Image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        n = len(image[0])
+        for row in image:
+            row = row.reverse()
+
+        for row in image:
+            for i in range(n):
+                if row[i] == 0:
+                    row[i] = 1
+                else:
+                    row[i] = 0
+        return image
