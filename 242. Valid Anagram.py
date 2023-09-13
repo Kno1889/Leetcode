5e661e74143b3afa2ca4f5d2cdb79a4559856d2b
Kno1889 <khaled.hossam1889@gmail.com>
2023-09-13 13:23:37 -0400

    Added problem 242

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        # O(nlogn) but O(1) space
+        return sorted(s) == sorted(t)
+        
+        # O(n) for time and memory
+        if len(s) != len(t):
+            return False
+        
+        # return sorted(s) == sorted(t)
+        lettersS = {}
+        lettersT = {}
+
+        for x in s:
+            lettersS[x] += 1
+        
+        for x in t:
+            lettersT[x] += 1
+        
+        return lettersS == lettersT
