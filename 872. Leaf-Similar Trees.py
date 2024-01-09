067856ae8f667453362dc11114b96a1673ab73dd
Kno1889 <khaled.hossam1889@gmail.com>
2024-01-09 13:16:49 -0500

    Added problem 872

---
+++ 872. Leaf-Similar Trees.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def traverse(root, sequence):
+            if not root.left and not root.right:
+                sequence.append(root.val)
+            else:
+                if root.left:
+                    traverse(root.left, sequence)
+                if root.right:
+                    traverse(root.right, sequence)
+
+        leaf_sequence_1, leaf_sequence_2 = [], []
+
+        traverse(root1, leaf_sequence_1)
+        traverse(root2, leaf_sequence_2)
+        
+        return leaf_sequence_1 == leaf_sequence_2
+        
