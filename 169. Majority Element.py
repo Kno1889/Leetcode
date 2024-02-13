d91244c1878ce882f9d0821c4cd5c8e4d851d46f
Kno1889 <khaled.hossam1889@gmail.com>
2024-02-13 11:24:13 -0500

    2nd approach to prob 169

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -1,5 +1,15 @@
-class Solution:
+class Solution1:
     def majorityElement(self, nums: List[int]) -> int:
 
         nums.sort()
         return nums[int(len(nums) / 2)]
+    
+class Solution2:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = {}
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+        
+        for num in counts:
+            if counts[num] >= len(nums) / 2:
+                return num
