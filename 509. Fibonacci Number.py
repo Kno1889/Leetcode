9dc841846eafdaa756f0fb7267b71a6637d7f8f0
Kno1889 <khaled.hossam1889@gmail.com>
2024-04-11 18:18:19 -0400

    problem 509

---
+++ 509. Fibonacci Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+
+        def fib_number(n):
+            if n == 0:
+                return 0
+            
+            if n == 1:
+                return 1
+            
+            return fib_number(n - 1) + fib_number(n - 2)
+    
+        return fib_number(n)
