85f2076bc11ad26312f3bb07ad5e6db2c3113771
Kno1889 <khaled.hossam1889@gmail.com>
2025-05-12 20:39:46 -0400

    Problem 49

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            if sorted_s not in anagrams:
+                anagrams[sorted_s] = [s]
+            else:
+                anagrams[sorted_s].append(s)
+
+        return list(anagrams.values())
+    # could also create a list of letter frequencies for each word and group accordingly
